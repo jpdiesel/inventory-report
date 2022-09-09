6ac4cf3b9dff425951c9f39ce196c37172d98b0a
jpdiesel <jpedro.diesel@gmail.com>
2022-09-09 19:19:50 -0300

    requirement 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        "Espada do Mihawk",
+        "OP",
+        "2022-09-09",
+        "2023-09-09",
+        "007",
+        "Tire das costas e sole"
+    )
+
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == "Espada do Mihawk"
+    assert new_product.nome_da_empresa == "OP"
+    assert new_product.data_de_fabricacao == "2022-09-09"
+    assert new_product.data_de_validade == "2023-09-09"
+    assert new_product.numero_de_serie == "007"
+    assert new_product.instrucoes_de_armazenamento == "Tire das costas e sole"
