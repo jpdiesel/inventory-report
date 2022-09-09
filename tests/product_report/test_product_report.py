580b35f254208a020e0c1de357ce062da9366e59
jpdiesel <jpedro.diesel@gmail.com>
2022-09-09 19:35:28 -0300

    requirement 2 done

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Espada do Mihawk",
+        "OP",
+        "2022-09-09",
+        "2023-09-09",
+        "007",
+        "Tire das costas e sole"
+    )
+
+    assert product.__repr__() == (
+        "O produto Espada do Mihawk"
+        " fabricado em 2022-09-09"
+        " por OP com validade"
+        " até 2023-09-09"
+        " precisa ser armazenado Tire das costas e sole."
+    )
