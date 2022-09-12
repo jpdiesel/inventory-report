575d6c8a7d715e2a9ff153b8829196d485b10a2e
jpdiesel <jpedro.diesel@gmail.com>
2022-09-12 17:49:10 -0300

    end

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,25 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        print("Verifique os argumentos", file=sys.stderr)
+    else:
+        file = sys.argv[1]
+        type = sys.argv[2]
+        if file.endswith(".json"):
+            inventory = InventoryRefactor(JsonImporter)
+        elif file.endswith(".csv"):
+            inventory = InventoryRefactor(CsvImporter)
+        else:
+            inventory = InventoryRefactor(XmlImporter)
+
+        print(inventory.import_data(file, type), end="")
+
+
+if __name__ == "__main__":
+    main()
